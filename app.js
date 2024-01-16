47aa90fff0cdc836a47246f58dd0580054b10235
bkm-code <144491053+bkm-code@users.noreply.github.com>
2024-01-17 01:48:54 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const sections = document.querySelectorAll(".section"); //Select sections.
 const sectionBtns = document.querySelectorAll(".controls"); //Select buttons parent container.
 const sectionBtn = document.querySelectorAll(".control") //Select individual buttons.
-const allSections = document.querySelectorAll(".main-content"); //Select body.
+const allSections = document.querySelector(".main-content"); //Select body; document.querySelector not document.querySelectorAll because addEventListener (used below) is a method for individual elements, not for collections. .
 
 function PageTransitions() {
     //Button click active class.
     for(let i = 0; i < sectionBtn.length; i++) {
-        sectionBtn[i].addEventListener("click", function() { //"function" instead of arrow function "=>" because this keyword doesn't exist in "=>".
+        sectionBtn[i].addEventListener("click", function () { //"function" instead of arrow function "=>" because this keyword doesn't exist in "=>".
             let currentBtn = document.querySelectorAll(".active-btn");
             currentBtn[0].className = currentBtn[0].className.replace("active-btn", "");
             this.className += "active-btn" //this keyword.
